4c9f12a5f186ddd076741ba91c02ffa4289fb99e
Mecid <102324700+mecit1997@users.noreply.github.com>
2023-01-10 21:29:16 +0300

    Update vehicle.cpp

--- vehicle.cpp
+++ vehicle.cpp
@@ -18,10 +18,10 @@ void left_wheel_stop();
 
 extern KonumDatalari_values_t KonumValues;
 
+//The haversine formula determines the great-circle distance between two points on a sphere given their longitudes and latitudes
 
-
-double dist(double th1, double ph1, double th2, double ph2){	//Haversine formülü, boylamları ve enlemleri verilen bir küre üzerindeki 
-																//iki nokta arasındaki büyük daire mesafesini belirler
+double dist(double th1, double ph1, double th2, double ph2){	 
+															
 	double dx, dy, dz;
 	ph1 -= ph2;
 	ph1 *= TO_RAD, th1 *= TO_RAD, th2 *= TO_RAD;				//radian to degree
@@ -33,6 +33,7 @@ double dist(double th1, double ph1, double th2, double ph2){	//Haversine formül
 	Serial.println(res);
 	return asin(sqrt(dx * dx + dy * dy + dz * dz) / 2) * 2 * R*1000;
 }
+/*This functions calculates the angle between two points (from current location to destination location)*/
 
 double calcAngle(double x1, double y1, double x2, double y2){
 	double X,Y;
@@ -53,60 +54,21 @@ double calcAngle(double x1, double y1, double x2, double y2){
 }
 
 
-void dest(char dest_lat_long[30]){      //bu fonksiyona telefondan gelecek enlem boylam bufferlanacak
-
-	char latitude_chr[15] , longitude_chr[15];
-	int i , j , latitude_int , longitude_int ;
-	float latitude_destination,longitude_destination;
-
-	//Getting Latitude
-	latitude_chr[0] = dest_lat_long[0];
-	latitude_chr[1] = dest_lat_long[1];
-
-	//Getting latitude values
-	for(i=3 ; i<10 ; i++){
-		latitude_chr[i-1] = dest_lat_long[i];
-	}
-	latitude_int = atoi(latitude_chr);
-
-
-
-	//Getting Longitude
-	longitude_chr[0] = dest_lat_long[10];
-	longitude_chr[1] = dest_lat_long[11];
-
-	//Getting longitude values
-	for(j=13 ; j<20 ; j++){
-		longitude_chr[j-11] = dest_lat_long[j];
-	}
-	longitude_int = atoi(longitude_chr);
-
-	//Integer to float
-	latitude_destination  = latitude_int;
-	longitude_destination = longitude_int;
-
-	//Converting float values
-
-	/*****************************************************************/
-	/*********************  Destination Values  **********************/
-	/*****************************************************************/
-	KonumValues.latitude_destination  = latitude_destination  / 1000000;
-	KonumValues.longitude_destination = longitude_destination / 1000000;
-
-}
-
+/*This function starts the right wheel of the vehicle*/
 void right_wheel_forward(){
   digitalWrite(motor2Pin1, HIGH);
   digitalWrite(motor2Pin2, LOW);
   //delay(1000);
 }
 
+/*This function starts the left wheel of the vehicle*/
 void left_wheel_forward(){
   digitalWrite(motor1Pin1, HIGH);
   digitalWrite(motor1Pin2, LOW);
 
 }
 
+/*This function starts both wheels so that vehicle moves forward*/
 void forward(){
   digitalWrite(motor1Pin1, HIGH);
   digitalWrite(motor1Pin2, LOW);
@@ -114,12 +76,14 @@ void forward(){
   digitalWrite(motor2Pin2, LOW);
 }
 
+/*This function stops right wheel*/
 void right_wheel_stop(){
   digitalWrite(motor2Pin1, LOW);
   digitalWrite(motor2Pin2, LOW);
 
 }
 
+/*This function stops left wheel*/
 void left_wheel_stop(){
   digitalWrite(motor1Pin1, LOW);
   digitalWrite(motor1Pin2, LOW);
